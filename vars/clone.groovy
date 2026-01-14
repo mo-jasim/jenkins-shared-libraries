44669e93072ddfd0c125d0c5bff4fb63433f5921
Mohammad Jasim <mrjasim87@gmail.com>
2026-01-14 21:04:44 +0530

    Add credentials parameter to git call function

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,5 +1,5 @@
-def call(String url, String branch) {
-    git url: "${url}", 
-        branch: "${branch}", 
-        credentialsId: 'mojasim' 
+def call(String url, String branch, String credentials) {
+    git url: "${url}",
+        branch: "${branch}",
+        credentialsId: "${credentials}"
 }
