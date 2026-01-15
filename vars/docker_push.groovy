8f945a777e4674cdb6af4f7aea510f83c30a2e5f
Mohammad Jasim <mrjasim87@gmail.com>
2026-01-15 11:53:00 +0530

    Modify Docker push script for credentials and tagging
    Updated default credentials variable and added tagging functionality for Docker images.

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,19 +1,43 @@
+// def call(Map config = [:]) {
+//     def imageName = config.imageName ?: error("Image name is required")
+//     def imageTag = config.imageTag ?: 'latest'
+//     def credentials = config.credentials ?: 'docker-hub-credentials'
+    
+//     echo "Pushing Docker image: ${imageName}:${imageTag}"
+    
+//     withCredentials([usernamePassword(
+//         credentialsId: credentials,
+//         usernameVariable: 'DOCKER_USERNAME',
+//         passwordVariable: 'DOCKER_PASSWORD'
+//     )]) {
+//         sh """
+//             echo "\$DOCKER_PASSWORD" | docker login -u "\$DOCKER_USERNAME" --password-stdin
+//             docker push ${imageName}:${imageTag}
+//             docker push ${imageName}:latest
+//         """
+//     }
+// }
+
 def call(Map config = [:]) {
     def imageName = config.imageName ?: error("Image name is required")
     def imageTag = config.imageTag ?: 'latest'
-    def credentials = config.credentials ?: 'docker-hub-credentials'
-    
+    def credentials = config.credentials ?: 'dockerCreds'
+
     echo "Pushing Docker image: ${imageName}:${imageTag}"
-    
+
     withCredentials([usernamePassword(
         credentialsId: credentials,
         usernameVariable: 'DOCKER_USERNAME',
         passwordVariable: 'DOCKER_PASSWORD'
     )]) {
         sh """
-            echo "\$DOCKER_PASSWORD" | docker login -u "\$DOCKER_USERNAME" --password-stdin
-            docker push ${imageName}:${imageTag}
-            docker push ${imageName}:latest
+            echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+            
+            // This is the missing part from your previous attempt
+            docker tag ${imageName}:${imageTag} \$DOCKER_USERNAME/${imageName}:${imageTag}
+            
+            // Push the version that includes your username
+            docker push \$DOCKER_USERNAME/${imageName}:${imageTag}
         """
     }
 }
